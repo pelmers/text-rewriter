1e0b8f61bf15ec559c011e4db99098dda4e3af96
Peter Elmers <peter.elmers@yahoo.com>
2015-03-22 14:16:16 -0500

    add reorder/removal button to preferences

--- data/prefs.js
+++ data/prefs.js
@@ -2,18 +2,25 @@ var tag = "rewrites",
     tag_out = "words_in",
     table = document.getElementById("pref_table"),
     save = document.getElementById("save_button"),
-    add = document.getElementById("add_button");
+    add = document.getElementById("add_button"),
+    // html code for adding sorting buttons to each row.
+    moveup = "<span id='moveup'>▲</span>",
+    delrow = "<span id='delrow'>☓</span>",
+    sortingHTML = "<span style='float:right;'>"+moveup+delrow+"</span>";
 
 // Make an input inside of a td element with given value and checked
-function makeTD(type, value, checked) {
+// Append stuff html code at the end of the input.
+function makeTD(type, value, checked, stuff) {
     var td = document.createElement('td'),
         inp = document.createElement('input');
     inp.type = type;
     if (value)
         inp.value = value;
-    if (checked !== undefined)
+    if (checked)
         inp.checked = checked;
-    td.appendChild(inp);
+    if (stuff)
+        td.innerHTML = stuff;
+    td.insertBefore(inp, td.firstChild)
     return td;
 }
 
@@ -23,18 +30,67 @@ function appendRow(data) {
         phrase = makeTD("text", data.from),
         replace = makeTD("text", data.to),
         case_box = makeTD("checkbox", null, data.ic),
-        whole_box = makeTD("checkbox", null, data.mw);
+        whole_box = makeTD("checkbox", null, data.mw, sortingHTML);
     tr.appendChild(phrase);
     tr.appendChild(replace);
     tr.appendChild(case_box);
     tr.appendChild(whole_box);
     table.appendChild(tr);
+    attachDelRowListener(tr.querySelector("#delrow"));
+    attachMoveUpListener(tr.querySelector("#moveup"));
 }
 
 // Append an empty row to the table.
 function appendEmptyRow() {
     appendRow({from:"", to:"", ic:false, mw:false});
 }
+
+// Call func(elem) on each element of arr.
+function forEach(arr, func) {
+    for (var i = 0; i < arr.length; i++)
+        func(arr[i]);
+}
+
+// Put b in front of a.
+function swapRows(a, b) {
+    b.parentNode.insertBefore(b, a);
+}
+
+// Listen on clicking up arrow for a row.
+function attachMoveUpListener(itm) {
+    (function(e) {
+        e.addEventListener('click', function() {
+            // can't move top element up
+            var thisRow = e.parentNode.parentNode.parentNode;
+            if (thisRow.rowIndex > 1) {
+                var prevRow = e.parentNode.parentNode.parentNode.previousSibling;
+                swapRows(prevRow, thisRow);
+            }
+        });
+    })(itm);
+}
+
+// Listen on clicking delete button of a row.
+function attachDelRowListener(itm) {
+    (function(e) {
+        e.addEventListener('click', function() {
+            table.deleteRow(e.parentNode.parentNode.parentNode.rowIndex);
+        });
+    })(itm);
+}
+
+// Receive data on port and add to the table.
+self.port.on(tag, function(data) {
+    var replacements = JSON.parse(data);
+    // put the data into the page
+    for (var i = 0; i < replacements.length; i++) {
+        appendRow(replacements[i]);
+    }
+    // make sure we have at least 3 rows in the table
+    for (var i = replacements.length; i < 3; i++) {
+        appendEmptyRow();
+    }
+});
 
 var saveTimeout;
 // Collect all row data and send back on port.
@@ -61,19 +117,4 @@ save.addEventListener('click', function() {
     }, 800);
 });
 
-add.addEventListener('click', function() {
-    appendEmptyRow();
-});
-
-// Receive data on port and add to the table.
-self.port.on(tag, function(data) {
-    var replacements = JSON.parse(data);
-    // put the data into the page
-    for (var i = 0; i < replacements.length; i++) {
-        appendRow(replacements[i]);
-    }
-    // make sure we have at least 3 rows in the table
-    for (var i = replacements.length; i < 3; i++) {
-        appendEmptyRow();
-    }
-});
+add.addEventListener('click', appendEmptyRow);
