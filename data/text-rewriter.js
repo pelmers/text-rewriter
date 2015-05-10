a04e9a3a78957bf7db9f606881684ec4fb80f671
Peter Elmers <peter.elmers@yahoo.com>
2015-05-10 12:25:04 -0500

    perform replacements on page title

--- data/text-rewriter.js
+++ data/text-rewriter.js
@@ -8,17 +8,21 @@ function makeRegex(rep) {
     return new RegExp(start+rep.from+end, "g"+ign);
 }
 
+// Call text.replace on each regex in replacements.
+function performReplacements(text, replacements) {
+    for (var i = 0; i < replacements.length; i++) {
+        text = text.replace(makeRegex(replacements[i]), replacements[i].to);
+    }
+    return text;
+}
+
 self.port.on(tag, function(data) {
     var replacements = JSON.parse(data),
         tree = document.createTreeWalker(document.body, NodeFilter.SHOW_TEXT, null, false),
         cur = tree.nextNode();
+    document.title = performReplacements(document.title, replacements);
     while (cur) {
-        var text = cur.nodeValue;
-        // perform the replacements
-        for (var i = 0; i < replacements.length; i++) {
-            text = text.replace(makeRegex(replacements[i]), replacements[i].to);
-        }
-        cur.nodeValue = text;
+        cur.nodeValue = performReplacements(cur.nodeValue, replacements);
         cur = tree.nextNode();
     }
 });
