fb80f4c361dda290991c71e17b1ba6f04bc58fa1
Peter Elmers <peter.elmers@yahoo.com>
2017-09-30 15:33:15 -0700

    Add counter badge to toolbar icon

--- data/text-rewriter.js
+++ data/text-rewriter.js
@@ -1,10 +1,5 @@
-// Firefox = browser, Chrome = chrome.
-let api;
-try {
-    api = chrome;
-} catch(e) {
-    api = browser;
-}
+// TODO: port to browser api
+const api = chrome;
 
 // Return regexp for the replacement.
 function makeRegex(rep) {
@@ -15,26 +10,38 @@ function makeRegex(rep) {
 }
 
 // Call text.replace on each regex in replacements.
+// Return {text, count} the new text and the number of replacements made.
 function performReplacements(text, replacements) {
+    let count = 0;
     for (let i = 0; i < replacements.length; i++) {
-        text = text.replace(makeRegex(replacements[i]), replacements[i].to);
+        const re = makeRegex(replacements[i]);
+        const matches = text.match(re);
+        count += (matches != null) ? matches.length : 0;
+        text = text.replace(re, replacements[i].to);
     }
-    return text;
+    return {text, count};
 }
 
 // Tell the background script a content page has loaded.
-api.runtime.sendMessage({"event": "pageLoad"});
+api.runtime.sendMessage({ event: "pageLoad" });
 
 api.runtime.onMessage.addListener(function (message) {
-    if (message.event === "textRewriter") {
+    const {event} = message;
+    if (event === "textRewriter") {
+        console.time("textRewriter");
         // handle response, which has a list of replacements
-        const replacements = message.replacements,
-              tree = document.createTreeWalker(document.body, NodeFilter.SHOW_TEXT, null, false);
-        document.title = performReplacements(document.title, replacements);
+        const {replacements, tabId} = message;
+        const tree = document.createTreeWalker(document.body, NodeFilter.SHOW_TEXT, null, false);
+        document.title = performReplacements(document.title, replacements).text;
         let cur = tree.nextNode();
+        let totalCount = 0;
         while (cur) {
-            cur.nodeValue = performReplacements(cur.nodeValue, replacements);
+            const {text, count} = performReplacements(cur.nodeValue, replacements);
+            cur.nodeValue = text;
+            totalCount += count;
             cur = tree.nextNode();
         }
+        console.timeEnd("textRewriter");
+        api.runtime.sendMessage({ event: "replaceCount" , totalCount, tabId });
     }
 });
