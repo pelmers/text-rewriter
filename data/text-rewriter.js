a7773f69a189ce0dd6c032b050587b0e57600765
Peter Elmers <peter.elmers@yahoo.com>
2017-10-31 22:00:53 -0700

    Dedup replacements, add sentence case

--- data/text-rewriter.js
+++ data/text-rewriter.js
@@ -1,47 +1,85 @@
 // TODO: port to browser api
 const api = chrome;
 
+const NODE_LIMIT = 50000;
+
 // this is some text => This Is Some Text
 function titleCase(str) {
-  const parts = str.split(' ');
-  for (let i = 0; i < parts.length; i++) {
-    if (parts[i].length > 0) {
-      parts[i] = parts[i].charAt(0).toUpperCase() + parts[i].slice(1);
+    const parts = str.split(' ');
+    for (let i = 0; i < parts.length; i++) {
+        if (parts[i].length > 0) {
+            parts[i] = parts[i].charAt(0).toUpperCase() + parts[i].slice(1);
+        }
     }
-  }
-  return parts.join(' ');
+    return parts.join(' ');
 }
+
+// this is some text => This is some text
+function sentenceCase(str) {
+    const parts = str.split(' ');
+    for (let i = 0; i < parts.length; i++) {
+        if (i === 0) {
+            parts[i] = titleCase(parts[i]);
+        } else {
+            parts[i] = parts[i].toLowerCase();
+        }
+    }
+    return parts.join(' ');
+}
+
+// delete duplicate copies of replacements entries.
+function dedup(replacements) {
+    const deduped = [];
+    for (let i = 0; i < replacements.length; i++) {
+        for (let j = i + 1; j < replacements.length; j++) {
+            const a = replacements[i];
+            const b = replacements[j];
+            if (a.from != b.from || a.to != b.to ||
+                a.ic != b.ic || a.mw != b.mw || a.sc != b.sc) {
+                deduped.push(a);
+            }
+        }
+    }
+    return deduped;
+}
+
 
 // Return new list of replacements with autocase replacements expanded.
 function expandAutoCaseReplacements(replacements) {
-    const newReplacements = [];
+    let newReplacements = [];
     for (let i = 0; i < replacements.length; i++) {
         const rep = replacements[i];
         if (rep.sc && !rep.ic) {
-            const repTitle = {
+            const smartCases = [
+            {
                 "from": titleCase(rep.from),
                 "to": titleCase(rep.to),
                 "ic": rep.ic,
                 "mw": rep.mw,
                 "sc": rep.sc,
-            };
-            const repUpper = {
+            },
+            {
+                "from": sentenceCase(rep.from),
+                "to": sentenceCase(rep.to),
+                "ic": rep.ic,
+                "mw": rep.mw,
+                "sc": rep.sc,
+            },
+            {
                 "from": rep.from.toUpperCase(),
                 "to": rep.to.toUpperCase(),
                 "ic": rep.ic,
                 "mw": rep.mw,
                 "sc": rep.sc,
-            };
-            const repLower = {
+            },
+            {
                 "from": rep.from.toLowerCase(),
                 "to": rep.to.toLowerCase(),
                 "ic": rep.ic,
                 "mw": rep.mw,
                 "sc": rep.sc,
-            };
-            newReplacements.push(repTitle);
-            newReplacements.push(repUpper);
-            newReplacements.push(repLower);
+            }];
+            newReplacements = newReplacements.concat(dedup(smartCases));
         } else {
             newReplacements.push(rep);
         }
@@ -76,7 +114,7 @@ api.runtime.sendMessage({ event: "pageLoad" });
 api.runtime.onMessage.addListener(function (message) {
     const {event} = message;
     if (event === "textRewriter") {
-        console.time("textRewriter");
+        console.time(event);
         // handle response, which has a list of replacements
         const {tabId} = message;
         const replacements = expandAutoCaseReplacements(message.replacements);
@@ -88,13 +126,18 @@ api.runtime.onMessage.addListener(function (message) {
         document.title = performReplacements(document.title, replacements).text;
         let cur = tree.nextNode();
         let totalCount = 0;
-        while (cur) {
-            const {text, count} = performReplacements(cur.nodeValue, replacements);
-            cur.nodeValue = text;
-            totalCount += count;
-            cur = tree.nextNode();
+        let visited = 0;
+        if (replacements.length > 0) {
+            while (cur != null && visited < NODE_LIMIT) {
+                const {text, count} = performReplacements(cur.nodeValue, replacements);
+                cur.nodeValue = text;
+                totalCount += count;
+                visited++;
+                cur = tree.nextNode();
+            }
         }
-        console.timeEnd("textRewriter");
+        console.timeEnd(event);
+        console.info(visited, "nodes visited");
         api.runtime.sendMessage({ event: "replaceCount" , totalCount, tabId });
     }
 });
